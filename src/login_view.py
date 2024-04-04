31e008d3b655fdbf82b561c623c563757d623e73
alev <alev@ad.helsinki.fi>
2024-04-04 12:46:31 +0300

    initializing login view

--- src/login_view.py
+++ src/login_view.py
@@ -1,26 +1,33 @@
 from tkinter import ttk, constants
 
 class LoginView:
-    def __init__(self, root, handle_login):
+    def __init__(self, root, handle_logout):
         self._root = root
-        self._handle_login = handle_login
-        self._username_entry = None
-        self._password_entry = None
-        self._error_variable = None
-        self._error_label = None
+        self._handle_logout = handle_logout
+        self._frame = None
+        # self._username_entry = None
+        # self._password_entry = None
+        # self._error_variable = None
+        # self._error_label = None
 
         self._initialize()
-    
-    def _iniatialize(self):
+
+    def pack(self):
+        self._frame.pack(fill = constants.X)
+
+    def destroy(self):
+        self._frame.destroy()
+
+    def _initialize(self):
         self._frame = ttk.Frame(master = self._root)
 
-        self._iniatialize_username_field()
-        self._iniatialize_password_field()
+        self._initialize_username_field()
+        self._initialize_password_field()
         login_button = ttk.Button(master = self._frame,
                                   text = "Login",
                                   command = self._login_handler)
+        self._frame.grid_columnconfigure(0, weight = 1, minsize = 400)
         login_button.grid(padx = 5, pady = 5, sticky = constants.EW)
-
         # add error message
 
     def _initialize_username_field(self):
@@ -35,3 +42,7 @@ class LoginView:
         password_label.grid(padx = 5, pady = 5, sticky = constants.W)
         self._password_entry.grid(padx = 5, pady = 5, sticky = constants.EW)
 
+    def _login_handler(self):
+        username = self._username_entry.get()
+        password = self._password_entry.get()
+        # create service.py where login is handled
