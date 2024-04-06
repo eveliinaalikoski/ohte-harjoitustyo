3f4e1b98c8c8738347d9b8923c46be5f77d614dd
alev <alev@ad.helsinki.fi>
2024-04-06 16:07:32 +0300

    file paths

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+dirname = os.path.dirname(__file__)
+
+try:
+    load_dotenv(dotenv_path = os.path.join(dirname, "..", ".env"))
+except FileNotFoundError:
+    pass
+
+BUDGET_FILENAME = os.getenv("BUDGET_FILENAME") or "data.csv"
+BUDGET_FILE_PATH = os.path.join(dirname, "..", "data", BUDGET_FILENAME)
+
+DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "database.sqlite"
+DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILENAME)
