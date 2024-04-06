2e1cc8f252ed7c924507a81783e2d846dab2671e
alev <alev@ad.helsinki.fi>
2024-04-06 17:23:10 +0300

    fixes

--- src/login_view.py
+++ src/login_view.py
@@ -1,12 +1,12 @@
 from tkinter import ttk, constants
 
 class LoginView:
-    def __init__(self, root, handle_logout):
+    def __init__(self, root, handle_login):
         self._root = root
-        self._handle_logout = handle_logout
+        self._handle_login = handle_login
         self._frame = None
-        # self._username_entry = None
-        # self._password_entry = None
+        self._username_entry = None
+        self._password_entry = None
         # self._error_variable = None
         # self._error_label = None
 
@@ -20,7 +20,6 @@ class LoginView:
 
     def _initialize(self):
         self._frame = ttk.Frame(master = self._root)
-
         self._initialize_username_field()
         self._initialize_password_field()
         login_button = ttk.Button(master = self._frame,
@@ -45,4 +44,4 @@ class LoginView:
     def _login_handler(self):
         username = self._username_entry.get()
         password = self._password_entry.get()
-        # create service.py where login is handled
+        self._handle_login()
