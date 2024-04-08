54c81906a1c42046be1476e9de8c0729fb8d8b93
alev <alev@ad.helsinki.fi>
2024-04-08 21:39:08 +0300

    removed comments

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -49,31 +49,3 @@ class UI:
         self._current_view = BudgetView(
             self._root, 
             self._show_login_view)
-
-    # def _show_create_user_view(self):
-    #     self._hide_current_view()
-    #     self._current_view = CreateUserView(
-    #         self._root,
-    #         self._show_budgets_view,
-    #         self._show_login_view)
-    #     self._current_view.pack()
-
-
-        # label = ttk.Label(master = self._root, 
-        #                   text = "BudgetBuddy")
-        # login_button = ttk.Button(master = self._root,
-        #                     text = "login")
-        # register_button = ttk.Button(master = self._root,
-        #                              text = "register")
-
-        # label.grid(row = 0, column = 0, columnspan = 2, 
-        #            sticky = constants.W,
-        #            padx = 5, pady = 5)
-        # login_button.grid(row = 2, column = 1, 
-        #                   sticky = (constants.E, constants.W),
-        #                   padx = 5, pady = 5)
-        # register_button.grid(row = 4, column = 1, 
-        #                      sticky = (constants.E, constants.W),
-        #                      padx = 5, pady = 5)
-
-        # self._root.grid_columnconfigure(1, weight = 1, minsize = 300)
