7d334abdcf59aaa6b1bde759429ae58d565622ce
alev <alev@ad.helsinki.fi>
2024-04-08 21:36:03 +0300

    added user repository

---
+++ src/repositories/user_repository.py
@@ -0,0 +1,22 @@
+from entities.user import User
+from database_connection import get_database_connection
+
+class UserRepository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    def create(self, user):
+        cursor = self._connection.cursor()
+        cursor.execute("""INSERT INTO users (username, password) 
+                       VALUES (?, ?)""", (user.username, user.password))
+        self._connection.commit()
+        return user
+    
+    def find_by_username(self, username):
+        cursor = self._connection.cursor()
+        row_id = cursor.execute("""SELECT * FROM users
+                       WHERE username = ?""", [username]).fetchone()
+        return row_id
+        
+
+user_repository = UserRepository(get_database_connection())
