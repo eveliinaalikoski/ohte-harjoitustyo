f3c363c500c11b266b3347c3eb549776b06ec989
alev <alev@ad.helsinki.fi>
2024-04-08 21:34:48 +0300

    added budget table

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -2,21 +2,31 @@ from database_connection import get_database_connection
 
 def drop_tables(connection):
     cursor = connection.cursor()
-    cursor.execute("drop table if exists users;")
+    cursor.execute("DROP TABLE if exists users;")
+    cursor.execute("DROP TABLE if exists budgets;")
     connection.commit()
 
-def create_tables(connection):
+def create_user_table(connection):
     cursor = connection.cursor()
-    cursor.execute("""create table users
-                   (id primary key,
-                   username text,
-                   password text);""")
+    cursor.execute("""CREATE TABLE users
+                   (id PRIMARY KEY,
+                   username TEXT UNIQUE,
+                   password TEXT);""")
+    connection.commit()
+
+def create_budget_table(connection):
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE budgets
+                   (id PRIMARY KEY,
+                   user_id INTEGER REFERENCES users);""")
+    # add all the budget topics in tables?
     connection.commit()
 
 def initialize_database():
     connection = get_database_connection()
     drop_tables(connection)
-    create_tables(connection)
+    create_user_table(connection)
+    create_budget_table(connection)
 
 if __name__=="__main__":
     initialize_database()
