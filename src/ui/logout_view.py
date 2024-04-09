b41cd028c3ef79bc13afa6f1f9f79a77ed768b6a
alev <alev@ad.helsinki.fi>
2024-04-09 22:04:48 +0300

    added initialization and logout handler

--- src/ui/logout_view.py
+++ src/ui/logout_view.py
@@ -1,4 +1,5 @@
 from tkinter import ttk, constants
+from services.budget_service import budget_service
 
 class LogoutView:
     def __init__(self, root, handle_logout):
@@ -13,3 +14,21 @@ class LogoutView:
 
     def destroy(self):
         self._frame.destroy()
+    
+    def _logout_handler(self):
+        budget_service.logout()
+        self._handle_logout()
+
+    def _initialize(self):
+        self._frame = ttk.Frame(master = self._root)
+        text = ttk.Label(master = self._frame,
+                         text = "You have successfully logged out :)",
+                         foreground = "green", 
+                         font = ("Arial", 15))
+        back_to_login_button = ttk.Button(master = self._frame,
+                                  text = "Back to login!",
+                                  command = self._logout_handler)
+        self._frame.grid_columnconfigure(0, weight = 1, minsize = 400)
+        text.grid(padx = 5, pady = 5, sticky = constants.EW)
+        back_to_login_button.grid(padx = 5, pady = 5, sticky = constants.EW)
+        
