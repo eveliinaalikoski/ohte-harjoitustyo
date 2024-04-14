419322749195ce11beb2605e99742793b8b81f08
alev <alev@ad.helsinki.fi>
2024-04-14 20:12:16 +0300

    deleted, wasn't needed

--- src/file_reader.py
+++
@@ -1,8 +0,0 @@
-import os
-
-dirname = os.path.dirname(__file__)
-data_file_path = os.path.join(dirname, "..", "data", "data.csv")
-
-with open(data_file_path) as file:
-    for row in file:
-        pass
