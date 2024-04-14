95ee3ac319fdf42aea1d4ed0967d170f09ee63fd
alev <alev@ad.helsinki.fi>
2024-04-14 20:17:28 +0300

    interface to see budget page

--- src/ui/budget_view.py
+++ src/ui/budget_view.py
@@ -1,11 +1,12 @@
 from tkinter import ttk, constants
 
 class BudgetView:
-    def __init__(self, root, handle_logout):
-        self.__root = root
-        self._handle_logout = handle_logout
+    def __init__(self, root, budget_name, to_front_page):
+        self._root = root
+        self._budget_name = budget_name
+        self._to_front_page = to_front_page
         self._window = None
-        # self._user = 
+
         self._budget_window()
     
     def pack(self):
@@ -14,10 +15,37 @@ class BudgetView:
     def destroy(self):
         self._window.destroy()
     
+    def _label(self):
+        front_button = ttk.Button(master = self._budget_frame,
+                                   text = "To front page",
+                                   command = self._to_front_page)
+        label = ttk.Label(master = self._budget_frame,
+                          text = self._budget_name,
+                          font = ("Arial", 25),
+                          foreground = "blue")
+        front_button.grid(row = 0, column = 0,
+                           padx = 5, pady = 5,
+                           sticky = constants.W)
+        label.grid(row = 2, column = 0,
+                   padx = 5, pady = 5,
+                   sticky = constants.EW)
+    
+    def _budget_info(self):
+        text = ttk.Label(master = self._budget_frame,
+                         text = "add info")
+        text.grid(row = 4, column = 0,
+                   padx = 5, pady = 5,
+                   sticky = constants.EW)
+    
     def _budget_window(self):
         self._window = ttk.Frame(master = self._root)
-        self._budget_list_frame = ttk.Frame(master = self._window)
-        self._budget_list_frame.grid(row = 1, column = 0,
+        self._budget_frame = ttk.Frame(master = self._window)
+    
+        self._label()
+        self._budget_info()
+
+        self._budget_frame.grid(row = 4, column = 0,
                                      columnspan = 2,
                                      sticky = (constants.EW))
-        
+        self._root.grid_columnconfigure(0, weight = 1, minsize = 400)
+        self._root.grid_columnconfigure(1, weight = 0)
