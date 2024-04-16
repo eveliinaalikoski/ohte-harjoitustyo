61405e0976afd1091e7fbe421d2365f998865182
alev <alev@ad.helsinki.fi>
2024-04-16 21:05:30 +0300

    added sql commands

--- src/repositories/budget_repository.py
+++ src/repositories/budget_repository.py
@@ -2,11 +2,14 @@ from entities.budget import Budget
 from repositories.user_repository import user_repository
 from config import BUDGET_FILE_PATH
 from pathlib import Path
+from database_connection import get_database_connection
+
 
 class BudgetRepository:
-    def __init__(self, file_path):
+    def __init__(self, file_path, connection):
         self._file_path = file_path
-    
+        self._connection = connection
+
     def _get_all(self):
         return self._read()
 
@@ -17,18 +20,25 @@ class BudgetRepository:
             part = row.split(";")
             budget_name = part[0]
             user = part[1]
-            income = part[2]
-            rent = part[3]
+            # income = part[2]
+            # rent = part[3]
+            # groceries = part[4]
+            # transportation = part[5]
+            # hobbies = part[6]
             if username == user:
-                own_budgets.append(Budget(budget_name, 
-                                      user, 
-                                      income, 
-                                      rent))
+                own_budgets.append(Budget(budget_name, user))
         return own_budgets
+
+    def get_by_budget_name(self, budget_name, username):
+        cursor = self._connection.cursor()
+        budget = cursor.execute("""SELECT * FROM budgets
+                       WHERE name = ? AND username = ?""", 
+                       (budget_name, username)).fetchall()
+        return budget
 
     def _file_exists(self):
         Path(self._file_path).touch()
-    
+
     def _read(self):
         self._file_exists()
         budget_list = []
@@ -37,26 +47,38 @@ class BudgetRepository:
                 row = row.replace("\n", "")
                 budget_list.append(row)
         return budget_list
-    
+
     def create_budget(self, budget):
         budgets = self._get_all()
         budgets.append(budget)
+        print(budgets)
         self._add(budget)
-    
+
+
     def _add(self, budget):
         self._file_exists()
         name = budget.name
         username = budget.username
-        income = str(budget.income)
-        rent = str(budget.rent)
+        # income = str(budget.income)
+        # rent = str(budget.rent)
+        # groceries = str(budget.groceries)
+        # transportation = str(budget.transportation)
+        # hobbies = str(budget.hobbies)
         with open(self._file_path, "a") as file:
             file.write((name)+";")
-            file.write((username)+";")
-            file.write((income)+";")
-            file.write((rent)+"\n")
+            file.write((username)+"\n")
+        
+        cursor = self._connection.cursor()
+        cursor.execute("""INSERT INTO budgets 
+                       (name, username, income, rent, groceries, 
+                       transportation, hobbies)
+                       VALUES (?, ?, 0, 0, 0, 0, 0)""",
+                       (name, username))
+        self._connection.commit()
 
     def delete(self):
         with open(self._file_path, "w") as file:
             file.write("")
 
-budget_repository = BudgetRepository(BUDGET_FILE_PATH)
+
+budget_repository = BudgetRepository(BUDGET_FILE_PATH, get_database_connection())
