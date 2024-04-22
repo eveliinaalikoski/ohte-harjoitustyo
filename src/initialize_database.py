ec4ee74c4671fe3a9af816b0fcb088e2888c7e24
alev <alev@ad.helsinki.fi>
2024-04-22 16:34:24 +0300

    added topics table

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -5,6 +5,7 @@ def drop_tables(connection):
     cursor = connection.cursor()
     cursor.execute("DROP TABLE if exists users;")
     cursor.execute("DROP TABLE if exists budgets;")
+    cursor.execute("DROP TABLE if exists topics;")
     connection.commit()
 
 
@@ -29,12 +30,20 @@ def create_budget_table(connection):
     # add all the budget topics in tables?
     connection.commit()
 
+def create_topics_table(connection):
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE topics
+                   (budget_name TEXT REFERENCES budgets,
+                   topic TEXT,
+                   amount INTEGER);""")
+    connection.commit()
 
 def initialize_database():
     connection = get_database_connection()
     drop_tables(connection)
     create_user_table(connection)
     create_budget_table(connection)
+    create_topics_table(connection)
 
 
 if __name__ == "__main__":
