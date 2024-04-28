d17205aaa85a0f27472eb5c6a7f0f054e0c00c8f
alev <alev@ad.helsinki.fi>
2024-04-28 19:39:11 +0300

    docstring

--- src/entities/budget.py
+++ src/entities/budget.py
@@ -1,7 +1,14 @@
 class Budget:
+    """class that represents a budget"""
     def __init__(self, name=None, username=None):
+        """class constructor, creates new budget
+
+        Args:
+            name (str, optional): 
+                name of the created budget. Defaults to None.
+            username (str, optional): 
+                username of the user that created budget.
+                Defaults to None.
+        """
         self.name = name
         self.username = username
-
-        # add more topics ?
-        # only name, username in this (?) and the ofter stuff to sql
--- src/entities/user.py
+++ src/entities/user.py
@@ -1,4 +1,11 @@
 class User:
+    """class that represents a user"""
     def __init__(self, username, password):
+        """class constructor, creates new user
+
+        Args:
+            username (str-string): user's username
+            password (str-string): user's password
+        """
         self.username = username
         self.password = password
